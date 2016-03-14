7cf8442288d3fb33577a0455108f9278dce0f6cb
Irene Alvarado <ire.alvarado@gmail.com>
2016-03-14 18:45:50 -0400

    Adding a few things to make script more robust:
    -Zoom level detail to picture names
    -Script doesn't fail of OSM times out in creating json file
    -Looks for json file in current directory in a way that works with older versions of OSX and windows

--- download_features.rb
+++ download_features.rb
@@ -7,22 +7,21 @@ require 'uri'
 require 'fileutils'
 require 'csv'
 
-
+DEFAULT_ZOOM = 17
 CONCURRENT_DOWNLOADS = 100
 SLEEP_BETWEEN_DOWNLOADS = 0
 BOTTOM_CROP = 23;
 EXPORT_PATH = "training_set/"
 IMAGE_SIZE = 256;
 CONTINENTAL_US = {s: 24.9493, w: -125.0011, n: 49.5904, e: -66.9326}
-DEFAULT_ZOOM = 18
 NUMBER_TO_DOWNLOAD = 1000
 
 $hydra = Typhoeus::Hydra.new
 
 $count = 0
 
-def get_json_from_openstreetmaps(key,val,constrain_to_usa = true)
-  timeout = 600
+def get_json_from_openstreetmaps(key,val,constrain_to_usa = false)
+  timeout = 20000
   bounds = "#{CONTINENTAL_US[:s]},#{CONTINENTAL_US[:w]},#{CONTINENTAL_US[:n]},#{CONTINENTAL_US[:e]}"
   bounds_string =  constrain_to_usa ? "(#{bounds})" : ""
   str = "data=[out:json][timeout:#{timeout}];node[\"#{key}\"=\"#{val}\"]#{bounds_string};out skel qt #{NUMBER_TO_DOWNLOAD};"
@@ -39,8 +38,7 @@ def build_url(lat,lng,zoom=DEFAULT_ZOOM, size=IMAGE_SIZE)
   str
 end
 
-def download_images(key,val,label, zoom=DEFAULT_ZOOM, usa_only = true)
-
+def download_images(key,val,label, zoom=DEFAULT_ZOOM, usa_only = false)
   dir = "#{EXPORT_PATH}#{label}"
   FileUtils.mkdir_p(dir)
 
@@ -52,14 +50,20 @@ def download_images(key,val,label, zoom=DEFAULT_ZOOM, usa_only = true)
     json_content = get_json_from_openstreetmaps(key,val,usa_only)
     File.open(json_path, "w+") {|f| f.puts json_content}
   end
-  elements = JSON.parse(json_content)["elements"]
+
+  #continue with next tag if OSM timed out
+  if(json_content.start_with?('<!DOCTYPE','<?xml')) 
+    return
+  else
+    elements = JSON.parse(json_content)["elements"]
+  end
 
   lookup = {}
   elements.each_slice(CONCURRENT_DOWNLOADS) do |slices|
     requests = []
     slices.each do |item|
       url = build_url(item["lat"],item["lon"], zoom)
-      filename = "#{label}_#{item["lat"]}_#{item["lon"]}.png"
+      filename = "#{label}_#{item["lat"]}_#{item["lon"]}_z#{DEFAULT_ZOOM}.png"
       lookup[url] = filename
       unless File.exist? "#{dir}/#{filename}"
         $count++
@@ -90,11 +94,12 @@ def download_images(key,val,label, zoom=DEFAULT_ZOOM, usa_only = true)
   end
 
   lookup.values.each do |filename|
-    path = "#{dir}/#{filename}"
+    path = "#{dir}/#{filename}\n"
+    print path
     if File.exist? path
       sizes = IO.read(path)[0x10..0x18].unpack('NN') # hack for getting sizes from the bytes of a PNG
       unless sizes[0] == IMAGE_SIZE && sizes[1] == IMAGE_SIZE
-        `mogrify -gravity north -extent  #{IMAGE_SIZE}x#{IMAGE_SIZE} #{path}`
+        `mogrify -gravity north -extent  #{IMAGE_SIZE}x#{IMAGE_SIZE} ./#{path}`
       end
     end
   end
