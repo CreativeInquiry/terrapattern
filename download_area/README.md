736e4c438649ec4d33031daeefbef34dfde19f7d
Irene Alvarado <ire.alvarado@gmail.com>
2016-05-24 23:46:17 -0400

    Adding example code for county download and name change to area

---
+++ download_area/README.md
@@ -0,0 +1,64 @@
+Download Area
+======
+
+This script helps you download map tiles at a certain level for a particular area as specified by a bounding box of points. It will first calculate how many map tiles to download for a particular zoom level and save those to a csv file called "points.csv". Then it will download a map tile for each point in the csv file. 
+
+You need to adjust two things to run the script: the bouding box for the points to download (starting on line 44) and zoom level / offset numbers (lines 14-19).
+
+Specifically, you need to specify and latitude and longitude offset corresponding to the angular distance that each tile covers for any given zoom level you wish to download tiles for. We provide the latitude and longitude offsets for zoom levels 18 and 19. You simply have to comment out or edit the relevant lines of code. 
+
+	# Adjust for different zoom levels
+	#DEFAULT_ZOOM = 18 
+	#lngOffset = 0.001386
+	#latOffset = 0.001373
+	
+	DEFAULT_ZOOM = 19
+	lngOffset = 0.000694
+	latOffset = 0.000686
+	
+The offsets are calculated as follows. You can use a quick trick assuming that:
+
+* 111,111 meters in the y direction is 1 degree of latitude. 
+* 111,111 * cos(latitude) meters in the x direction is 1 degree of longitude
+
+You also need to know that:
+
+* Each map tile is 256x256 pixels
+* Each zoom level features a resolution defined in meteres per pixel. You can find a list of resolutions [here](http://wiki.openstreetmap.org/wiki/Zoom_levels). For level 19, the resolution is 0.298 meters / pixel. 
+
+To calculate the latitude offset use the formula:
+
+	((resolution * length of tile) * 1) / 111111 
+		
+at zoom level 19 that would be:
+
+	(0.298 * 256) * 1 / 111111 = 0.00068659268
+	
+To calculate the longitude offset use the formula:
+
+	((resolution * height of tile) * 1) / (111111 * cos(latitude))
+	
+at zoom level 19, for an example latitude of 26.624824 that would be
+
+	((0.298 * 256) * 1) / (111111 * cos(26.624824)) = 0.0087316879
+
+## Where do these numbers come from?
+
+Well, let's go back to history. The origins of the meter date back to the 18th century, to a time when there were two competing standards for a unit of length. One approach suggested defining a meter as the length of a pendulum having a half-period of one second. Another approach suggested using the circumference of the earth: a meter would equal one ten-millionth of the length of the earth's meridian along a quadrant. 
+
+The latter approach won and the meter was defined soon after the French Revolution as equal to 10^-7 or one ten-millionth of the length of the meridian through Paris from pole to the equator. In other words, 10^7 meters would be the distance along the Paris meridian from the equator to the north pole. 
+
+What follows is that 10^7 meters / 90 degrees = 111,111.111111 meters / degree.
+
+Source: [http://physics.nist.gov/cuu/Units/meter.html](http://physics.nist.gov/cuu/Units/meter.html)
+
+
+## How to run
+
+Assuming you have set your environment variables in a .env file in the parent directory, simply run
+
+	ruby download_area/download_area.rb
+
+## Resources
+
+[geojson.io](http://geojson.io/) is a useful app for visualizing and editing geojson files.
---
+++ download_area/download_area.rb
@@ -0,0 +1,230 @@
+# Example code for about 800 square miles around Miami
+
+require 'rgeo'
+require 'rgeo/geo_json'
+require 'json'
+require 'typhoeus'
+require 'dotenv'
+require 'uri'
+require 'fileutils'
+
+Dotenv.load
+
+# Adjust for different zoom levels
+#DEFAULT_ZOOM = 18 
+#lngOffset = 0.001386
+#latOffset = 0.001373
+DEFAULT_ZOOM = 19
+lngOffset = 0.000694
+latOffset = 0.000686
+
+dir = "#{File.dirname(__FILE__)}/results" # Directory to save a csv file with all the lat lon points to download tiles for
+BOTTOM_CROP = 23
+IMAGE_SIZE = 256
+CONCURRENT_DOWNLOADS = 100
+SLEEP_BETWEEN_DOWNLOADS = 0
+
+FileUtils.mkdir_p(dir)
+
+$hydra = Typhoeus::Hydra.new
+
+
+# Adjust this geojson section to represent the bounding box for area to download
+str = <<GEOJSON
+{
+  "type": "Feature",
+  "properties": {
+    "NAME": "Miami"
+  },
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          -80.37460327148438,
+          26.154205294151907
+        ],
+        [
+          -80.30593872070311,
+          26.22444694563432
+        ],
+        [
+          -80.068359375,
+          26.220751072791945
+        ],
+        [
+          -80.101318359375,
+          25.961748853879143
+        ],
+        [
+          -80.09857177734375,
+          25.77887050485261
+        ],
+        [
+          -80.145263671875,
+          25.64647846279615
+        ],
+        [
+          -80.16448974609375,
+          25.61304787081554
+        ],
+        [
+          -80.22628784179688,
+          25.65761991333506
+        ],
+        [
+          -80.26336669921875,
+          25.601902261115754
+        ],
+        [
+          -80.51742553710938,
+          25.70959958489245
+        ],
+        [
+          -80.50643920898438,
+          25.91482062206972
+        ],
+        [
+          -80.45974731445312,
+          25.96915686519401
+        ],
+        [
+          -80.46524047851562,
+          26.048146607649734
+        ],
+        [
+          -80.48309326171875,
+          26.085154746196096
+        ],
+        [
+          -80.47760009765624,
+          26.177623883345746
+        ],
+        [
+          -80.38284301757812,
+          26.154205294151907
+        ],
+        [
+          -80.37460327148438,
+          26.154205294151907
+        ]
+      ]
+    ]
+  }
+}
+GEOJSON
+
+
+## This finds the bounding box of the geojson.  
+p0 = JSON.parse(str)["geometry"]["coordinates"].first.first
+maxLat, maxLng = p0
+minLat, minLng = p0
+JSON.parse(str)["geometry"]["coordinates"].first.each do |lat,lng|
+  maxLat = lat if lat > maxLat
+  print "maxLat ", maxLat, "  "
+  minLat = lat if lat < minLat
+  print "minLat ", minLat, "  "
+  maxLng = lng if lng > maxLng
+  print "maxLng ", maxLng, "  "
+  minLng = lng if lng < minLng
+  print "minLng ", minLng, "  "
+  print "\n\n"
+end
+
+
+# This is the silly Java-ish initialization for the geography engine
+factory = ::RGeo::Geos.factory
+
+# This parses the geoJSON and turns it into a gemoetry object
+feature = RGeo::GeoJSON.decode(str, json_parser: :json)
+county_boundary = feature.geometry
+
+saved_points = []
+
+path = "#{dir}/points.csv"
+
+if File.exist? path
+  File.foreach(path) do |line|
+    currentLat, currentLng = line.split(",")
+    next if currentLat == "lat"
+    saved_points.push({lat: currentLat.to_f, lng: currentLng.to_f})
+  end
+else
+  # This opens the file and puts in a header 
+  f = File.open(path, "w")
+  f.puts "lat,lng"
+
+  # This iterates through the points and adds a lat/lng pair
+  # to the CSV for each point
+  currentLat = minLat
+  while currentLat <= maxLat
+    currentLng = minLng
+    while currentLng <= maxLng
+      point = factory.point(currentLat,currentLng)
+      if point.within?(county_boundary)
+        f.puts "#{currentLng.round(7)},#{currentLat.round(7)}"
+        saved_points.push({lng: currentLat.round(7), lat: currentLng.round(7)})
+      end
+      currentLng += lngOffset/2
+    end
+    currentLat += latOffset/2
+  end
+
+  # Clean up after yourself, says your mother.
+  f.close
+end
+#--- 
+def build_url(lat,lng,zoom=DEFAULT_ZOOM, size=IMAGE_SIZE)
+  str = 'https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center='
+  str << "#{lat},#{lng}&zoom=#{zoom}&size=#{size}x#{size+BOTTOM_CROP}&key=#{ENV['GMAPS_KEY']}"
+  str
+end
+
+lookup = {}
+label = JSON.parse(str)["properties"]["NAME"]
+
+# THis breaks them into batches
+saved_points.each_slice(CONCURRENT_DOWNLOADS) do |slices|
+#saved_points[0..20].each_slice(CONCURRENT_DOWNLOADS) do |slices|
+  requests = []
+  slices.each do |item|
+    lat = item[:lat]
+    lng = item[:lng]
+    url = build_url(lat,lng, DEFAULT_ZOOM)
+    filename = "#{label}_#{lat}_#{lng}_z#{DEFAULT_ZOOM}.png"
+    lookup[url] = filename
+    unless File.exist? "#{dir}/#{filename}"
+      request = Typhoeus::Request.new(url, {followlocation: true, timeout: 300})
+      $hydra.queue(request)
+      requests.push request
+    else
+      #puts "skipping #{filename}"
+    end
+  end
+
+  ## DOWNLOAD THE MAP TILES
+  $hydra.run
+
+  ## SAVE THEM TO DISK
+  responses = requests.map { |r|
+    puts "#{r.url}: #{r.response.status_message}"
+    if r.response.status_message == 'OK' &&  r.response.body.size > 5000
+      File.open("#{dir}/#{lookup[r.url]}","wb") do |f|
+        f.puts r.response.response_body
+      end
+    end
+  }
+  unless requests.empty?
+    sleep(SLEEP_BETWEEN_DOWNLOADS)
+  end
+end
+
+lookup.values.each do |filename|
+  path = "#{dir}/#{filename}"
+  if File.exist? path
+    sizes = IO.read(path)[0x10..0x18].unpack('NN') # hack for getting sizes from the bytes of a PNG
+    unless sizes[0] == IMAGE_SIZE && sizes[1] == IMAGE_SIZE
+      `mogrify -gravity north -extent  #{IMAGE_SIZE}x#{IMAGE_SIZE} #{path}`
+    end
+  end
+end
